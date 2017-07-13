967dd6b61423fc6d09b696203a67475eb6d315ca
isaiahMASC4 <isaiahmassey6@gmail.com>
2017-07-13 09:20:39 -0400

    conditionals and functions

---
+++ condition.js
@@ -0,0 +1,1 @@
+var level = 11; if (level%2==0){console.log("you're on an even level!");}else{console.log("you're on an odd level!");}
---
+++ functions.js
@@ -0,0 +1,12 @@
+// function wellohorld (){
+//     console.log("Wello Horld");
+// }
+
+// wellohorld()
+
+function welloHorld(){
+    console.log("wello Horld");
+    return "wello Horld";
+}
+
+welloHorld();
